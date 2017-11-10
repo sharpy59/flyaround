133a1cf570178903e61c112137b8977f695890ac
sharpy59 <jason59270@gmail.com>
2017-11-10 16:11:36 +0100

    form quest

--- src/WCS/CoavBundle/Form/ReviewType.php
+++ src/WCS/CoavBundle/Form/ReviewType.php
@@ -2,21 +2,40 @@
 
 namespace WCS\CoavBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReviewType extends AbstractType
 {
     /**
-     * {@inheritdoc}
+     * {@inheritdoc} Including all fields from Review entity.
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder
+            ->add('text',TextareaType::class, array('attr' => array('maxlength' => 250, 'label' => 'Description')))
+            ->add('publicationDate', DateType::class, array('data' => new \DateTime('now')))
+            ->add('note', IntegerType::class, array('attr' => array('min' => 0, 'max' => 5, 'label' => 'note')))
+            ->add('agreeTerms', CheckboxType::class, array('mapped' => false))
+            ->add('userRated', EntityType::class, array(
+                'class' => 'WCS\CoavBundle\Entity\User',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.lastName', 'ASC');
+                },
+                'choice_label' => 'phoneNumber'))
+        ->add('reviewAuthor');
     }
     
     /**
-     * {@inheritdoc}
+     * {@inheritdoc} Targeting Review entity.
      */
     public function configureOptions(OptionsResolver $resolver)
     {
